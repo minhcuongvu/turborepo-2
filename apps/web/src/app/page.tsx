7b3e96b2d7f101e2715e0da3fd8fa294aa351abd
Adrian <noedigsti@gmail.com>
2024-07-03 11:28:20 +1000

    add tailwind to ui

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,9 +1,46 @@
-import Image from "next/image";
+import Image from 'next/image';
+import { Card } from '@repo/ui/card';
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     Hello
+      <div>Hello</div>
+      <div className="grid mb-32 text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+        {LINKS.map(({ title, href, description }) => (
+          <Card
+            className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-neutral-700 hover:bg-neutral-800/30"
+            href={href}
+            key={title}
+            title={title}
+          >
+            {description}
+          </Card>
+        ))}
+      </div>
     </main>
   );
 }
+
+const LINKS = [
+  {
+    title: 'Docs',
+    href: 'https://turbo.build/repo/docs',
+    description: 'Find in-depth information about Turborepo features and API.',
+  },
+  {
+    title: 'Learn',
+    href: 'https://turbo.build/repo/docs/handbook',
+    description: 'Learn more about monorepos with our handbook.',
+  },
+  {
+    title: 'Templates',
+    href: 'https://turbo.build/repo/docs/getting-started/from-example',
+    description: 'Choose from over 15 examples and deploy with a single click.',
+  },
+  {
+    title: 'Deploy',
+    href: 'https://vercel.com/new',
+    description:
+      'Instantly deploy your Turborepo to a shareable URL with Vercel.',
+  },
+];
