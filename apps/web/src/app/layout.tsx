ce9d6e1e2d77bb4832c82c0a4c0735b4503ca545
Adrian <noedigsti@gmail.com>
2024-12-01 17:49:03 +1000

    add speed insights

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import './globals.css';
 import '@repo/ui/styles.css';
 import StoreProvider from './StoreProvider';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 export const metadata: Metadata = {
   title: {
     template: '%s | App',
@@ -18,6 +19,7 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body>
         <StoreProvider>{children}</StoreProvider>
+        <SpeedInsights />
       </body>
     </html>
   );
