4000da62818a6393be7c5ec9404a4f40e98c43e0
Michael <noedigsti@gmail.com>
2025-06-17 03:08:03 +1000

    theme switcher, keep it simpler

--- apps/web/src/components/layout/navbar/index.tsx
+++ apps/web/src/components/layout/navbar/index.tsx
@@ -6,7 +6,14 @@ import {
 } from '@/lib/features';
 import { useAppDispatch, useAppSelector } from '@/lib/hooks';
 import { LaptopIcon, MoonIcon, SunIcon } from '@radix-ui/react-icons';
-import { Button, Container, Flex, IconButton, Text, Tooltip } from '@radix-ui/themes';
+import {
+  Button,
+  Container,
+  Flex,
+  IconButton,
+  Text,
+  Tooltip,
+} from '@radix-ui/themes';
 import { AccentColorSwitcherDropdown, Navbar } from '@repo/ui/components';
 import { AccentColor, AccentColorOptions } from '@repo/ui/interfaces';
 import { useTheme } from 'next-themes';
@@ -40,59 +47,60 @@ export default function NavComponent() {
 
   return (
     <Navbar>
-      <Flex className="flex absolute right-0" pr="4" gap="4" align="center">
+      <Flex className="flex absolute right-0 top-0" pr="4" pt="2" align="center">
         {isThemeReady && (
-          <div className="grid grid-flow-col border border-neutral-400 rounded-full">
-            <input
-              type="radio"
-              id="html"
-              name="theme"
-              value="system"
-              onClick={() => setTheme('system')}
-              defaultChecked={resolvedTheme === 'system'}
-              className="sr-only"
-            />
-            <label
-              htmlFor="html"
-              className="px-2 py-1 cursor-pointer text-neutral-400 hover:text-neutral-900 dark:text-neutral-400 dark:hover:text-neutral-100"
-            >
-              <LaptopIcon className="w-5 h-5" />
-              {/* <span className="text-sm">System</span> */}
-            </label>
-
-            <input
-              type="radio"
-              id="css"
-              name="theme"
-              value="dark"
-              onClick={() => setTheme('dark')}
-              defaultChecked={resolvedTheme === 'dark'}
-              className="sr-only"
-            />
-            <label
-              htmlFor="css"
-              className="px-2 py-1 cursor-pointer text-neutral-400 hover:text-neutral-900 dark:text-neutral-400 dark:hover:text-neutral-100"
-            >
-              <MoonIcon className="w-5 h-5" />
-              {/* <span className="text-sm">Dark</span> */}
-            </label>
-
-            <input
-              type="radio"
-              id="javascript"
-              name="theme"
-              value="light"
-              onClick={() => setTheme('light')}
-              defaultChecked={resolvedTheme === 'light'}
-              className="sr-only"
-            />
-            <label
-              htmlFor="javascript"
-              className="px-2 py-1 cursor-pointer text-neutral-400 hover:text-neutral-900 dark:text-neutral-400 dark:hover:text-neutral-100"
-            >
-              <SunIcon className="w-5 h-5" />
-              {/* <span className="text-sm">Light</span> */}
-            </label>
+          <div className={`${styles.tabs} flex flex-row gap-2 border border-neutral-400 rounded-lg`}>
+            <div className="flex items-center justify-center">
+              <input
+                type="radio"
+                id="html"
+                name="theme"
+                value="system"
+                onClick={() => setTheme('system')}
+                defaultChecked={resolvedTheme === 'system'}
+                className="sr-only peer"
+              />
+              <label
+                htmlFor="html"
+                className="p-1 peer-checked:text-neutral-100 cursor-pointer text-neutral-400 hover:text-neutral-900 dark:text-neutral-400 dark:hover:text-neutral-100"
+              >
+                <LaptopIcon className="w-5 h-5" />
+              </label>
+            </div>
+            <div className="flex items-center justify-center">
+              <input
+                type="radio"
+                id="css"
+                name="theme"
+                value="dark"
+                onClick={() => setTheme('dark')}
+                defaultChecked={resolvedTheme === 'dark'}
+                className="sr-only peer"
+              />
+              <label
+                htmlFor="css"
+                className="p-1 peer-checked:text-neutral-100 cursor-pointer text-neutral-400 hover:text-neutral-900 dark:text-neutral-400 dark:hover:text-neutral-100"
+              >
+                <MoonIcon className="w-5 h-5" />
+              </label>
+            </div>
+            <div className="flex items-center justify-center">
+              <input
+                type="radio"
+                id="javascript"
+                name="theme"
+                value="light"
+                onClick={() => setTheme('light')}
+                defaultChecked={resolvedTheme === 'light'}
+                className="sr-only peer"
+              />
+              <label
+                htmlFor="javascript"
+                className="p-1 peer-checked:text-neutral-900 cursor-pointer text-neutral-400 hover:text-neutral-900 dark:text-neutral-400 dark:hover:text-neutral-100"
+              >
+                <SunIcon className="w-5 h-5" />
+              </label>
+            </div>
           </div>
         )}
       </Flex>
